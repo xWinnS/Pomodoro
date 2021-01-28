4dc7ca358362c34a49465bb53a232333f94b7c18
xWinnS <pablowellerson18@gmail.com>
2021-01-27 21:10:43 -0300

    Gear and layout

--- js/script.js
+++ js/script.js
@@ -12,7 +12,26 @@ function setProgress(percent) {
 
 setProgress(100);
 
+const startPause = document.querySelector('.pause');
+startPause.addEventListener("click", toggleStart);
 
+function toggleStart(){
+    if(startPause.innerHTML == "START"){
+        startPause.innerHTML = "PAUSE";
+        startTime();
+    }else if(startPause.innerHTML == "PAUSE"){
+        startPause.innerHTML = "START";
+        pauseTime();
+    }
+}
+
+function startTime() {
+
+}
+
+function pauseTime() {
+    
+}
 
 
 /* ------------------- */
@@ -24,26 +43,60 @@ const time_p = document.querySelector('.time');
 
 pomodoro.addEventListener("click", function(){
     time_p.innerHTML = "25:00";
-    pomodoro.style.backgroundColor = "#F87073";
-    pomodoro.style.color = "#212645";
+    pomodoro.classList.add('selecionada');
+    short_break.classList.remove('selecionada');
+    long_break.classList.remove('selecionada');
 });
 
 short_break.addEventListener("click", function(){
     time_p.innerHTML = "05:00";
-    short_break.style.backgroundColor = "#F87073";
-    short_break.style.color = "#212645";
+    short_break.classList.add('selecionada');
+    pomodoro.classList.remove('selecionada');
+    long_break.classList.remove('selecionada');
+});
+
+long_break.addEventListener("click", function(){
+    time_p.innerHTML = "30:00";
+    long_break.classList.add('selecionada');
+    pomodoro.classList.remove('selecionada');
+    short_break.classList.remove('selecionada');
 });
 
 
 
 const config = document.querySelector('.config');
+const config_background = document.querySelector('.config_background');
 const btn = document.querySelector('.btn_ok');
 const gear = document.querySelector('.gear');
+const minutos = document.querySelector('.minutos');
+const segundos = document.querySelector('.segundos');
+
+
+config_background.addEventListener("click",function(){
+    config.classList.toggle("hidden");
+});
 
 btn.addEventListener("click",function(){
     config.classList.toggle("hidden");
+    alteraTempo();
 });
 
 gear.addEventListener("click",function(){
     config.classList.toggle("hidden");
 });
+
+function alteraTempo(){
+    let minuto = minutos.value;
+    let segundo = segundos.value;
+    if(minuto > 60){
+        minuto = "60";
+    }
+    if(segundo > 59){
+        segundo =  "59";
+    }
+    if(minuto == 60) {
+        segundo = "00";
+    }
+    let time = minuto + ":" + segundo;
+    time_p.innerHTML = time;
+}
